35d8719e0908984c02ee52bc9201bce4b1286aba
nobiot <noboru.ota@gmail.com>
2019-12-14 14:19:11 +0100

    JLS logo, font, etc

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,31 +2,31 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 const Header = (props) => (
-    <header id="header" style={props.timeout ? {display: 'none'} : {}}>
-        <div className="logo">
-            <span className="icon fa-diamond"></span>
-        </div>
-        <div className="content">
-            <div className="inner">
-                <h1>Dimension</h1>
-                <p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
-                for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
-            </div>
-        </div>
-        <nav>
-            <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
-            </ul>
-        </nav>
-    </header>
+  <header id='header' style={props.timeout ? { display: 'none' } : {}}>
+    <div className='logo'>
+      <img src='./images/emblem_white.png' height='80' className='icon' />
+    </div>
+    <div className='content'>
+      <div className='inner'>
+        <h1>自由が丘ランゲージスクールへようこそ。</h1>
+        <h2>自由が丘駅徒歩３０秒。</h2>
+        <h3>お問合せ・お申込みは 0120-009-035 (フリーダイアル)</h3>
+      </div>
+    </div>
+    <nav>
+      <ul>
+        <li><a href='javascript:;' onClick={() => { props.onOpenArticle('intro') }}>Intro</a></li>
+        <li><a href='javascript:;' onClick={() => { props.onOpenArticle('work') }}>Work</a></li>
+        <li><a href='javascript:;' onClick={() => { props.onOpenArticle('about') }}>About</a></li>
+        <li><a href='javascript:;' onClick={() => { props.onOpenArticle('contact') }}>Contact</a></li>
+      </ul>
+    </nav>
+  </header>
 )
 
 Header.propTypes = {
-    onOpenArticle: PropTypes.func,
-    timeout: PropTypes.bool
+  onOpenArticle: PropTypes.func,
+  timeout: PropTypes.bool
 }
 
 export default Header
